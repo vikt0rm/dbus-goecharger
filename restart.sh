ce71a8adbc1be95b3f8e06249a1e8cbc2f8ad967
Viktor Markstädter <viktoorm@gmail.com>
2022-03-30 13:16:57 +0200

    add service scripts

---
+++ restart.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+kill $(pgrep -f "python $SCRIPT_DIR/dbus-goecharger-acload.py")
---
+++ uninstall.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+SERVICE_NAME=$(basename $SCRIPT_DIR)
+
+rm /service/$SERVICE_NAME
+kill $(pgrep -f 'supervise dbus-goecharger-acload')
+chmod a-x $SCRIPT_DIR/service/run
+./restart.sh
